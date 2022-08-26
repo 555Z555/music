6d2b64176350f5e0ed7be02a1ec375cf74eeb00c
555Z555 <73090843+555Z555@users.noreply.github.com>
2022-08-26 14:05:54 +0800

    Update sukeTV.py

--- scripts/sukeTV.py
+++ scripts/sukeTV.py
@@ -15,13 +15,13 @@ def grab(url):
         response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://raw.githubusercontent.com/user-name/repo-name/main/assets/info.m3u8')
+                print('https://raw.githubusercontent.com/555Z555/music/main/assets/info.m3u8')
                 return
             #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/user-name/repo-name/main/assets/info.m3u8')
+                print('https://raw.githubusercontent.com/555Z555/music/main/assets/info.m3u8')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
